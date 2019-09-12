0a321832fe93748220bfbb504dc9001bd48ad204
Prabhjot Mattu <theprabh@gmail.com>
2019-09-12 01:03:28 -0500

    New UI
    Switched to new UI, added tableView for New Game

---
+++ Views/PeriodLengthViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PeriodLengthViewController.swift
+//  BasketballTimekeeper
+//
+//  Created by Prabhjot S. Mattu on 9/12/19.
+//  Copyright © 2019 Prabhjot S. Mattu. All rights reserved.
+//
+
+import UIKit
+
+class PeriodLengthViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNeedsStatusBarAppearanceUpdate()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
